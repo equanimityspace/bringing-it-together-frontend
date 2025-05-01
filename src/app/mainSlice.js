2cdeca1459b69f969edc978d3787e88cd68efa26
Ryan <ryfly2016@gmail.com>
2025-05-01 10:24:13 -0600

    add slices and api

---
+++ src/app/mainSlice.js
@@ -0,0 +1,55 @@
+import api from "../api/api";
+
+// login, register, get all users, get user by id
+const usersApi = api.injectEndpoints({
+  endpoints: (build) => ({
+    register: build.mutation({
+      query: ({ email, firstName, lastName, password }) => ({
+        url: "/register",
+        method: "POST",
+        body: {
+          email,
+          firstName,
+          lastName,
+          password,
+        },
+      }),
+      providesTags: ["User"],
+    }),
+
+    login: build.mutation({
+      query: ({ email, password }) => ({
+        url: "/login",
+        method: "POST",
+        body: {
+          email,
+          password,
+        },
+      }),
+      providesTags: ["User"],
+    }),
+
+    getAllUsers: build.query({
+      query: () => ({
+        url: "/",
+        method: "GET",
+      }),
+      providesTags: ["User"],
+    }),
+
+    getUser: build.query({
+      query: (id) => ({
+        url: `users/${id}`,
+        method: "GET",
+      }),
+      invalidatesTags: ["User"],
+    }),
+  }),
+});
+
+export const {
+  useRegisterMutation,
+  useLoginMutation,
+  useGetAllUsersQuery,
+  useGetUserQuery,
+} = usersApi;
