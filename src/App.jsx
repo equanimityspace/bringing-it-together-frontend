c221c174729f635ca3a252442eb00763137fdc3a
Ryan <ryfly2016@gmail.com>
2025-05-01 11:49:34 -0600

    fix boiler plate code

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,25 @@
+import "bootstrap/dist/css/bootstrap.min.css";
 import { Routes, Route } from "react-router-dom";
 
-import { Home } from "./components/Home";
-import { Login } from "./components/Login";
-import { Registration } from "./components/Registration";
-import { SingleUser } from "./components/SingleUser";
+// import Home from "./components/Home";
+import Login from "./components/Login";
+// import Registration from "./components/Registration";
+// import SingleUser from "./components/SingleUser";
 
 function App() {
-  <>
-    <Routes>
-      {/* No login needed */}
-      <Route path="/register" element={<Registration />} />
-      <Route path="/login" element={<Login />} />
+  return (
+    <>
+      <Routes>
+        {/* No login needed */}
+        {/* <Route path="/register" element={<Registration />} /> */}
+        <Route path="/login" element={<Login />} />
 
-      {/* must be logged in */}
-      <Route path="/" element={<Home />} />
-      <Route path="/user/:id" element={<SingleUser />} />
-    </Routes>
-  </>;
+        {/* must be logged in */}
+        {/* <Route path="/" element={<Home />} />
+      <Route path="/user/:id" element={<SingleUser />} /> */}
+      </Routes>
+    </>
+  );
 }
 
 export default App;
