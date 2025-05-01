7690009363c06c1762733706e54c2dd46c834f56
Ryan <ryfly2016@gmail.com>
2025-05-01 12:31:07 -0600

    fix functions in mainSlice.js not being recognized as functions

--- src/api/api.js
+++ src/api/api.js
@@ -1,4 +1,4 @@
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query";
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { getToken } from "../app/tokenService";
 
 const BASE_URL = "https://bring-it-all-together-backend-61a2.onrender.com";
