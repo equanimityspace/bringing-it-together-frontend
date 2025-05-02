12dde7b3bca8dec3d58c5d2bfb8d17d058ecc666
heatherd2025 <heatherdeliso@gmail.com>
2025-05-02 14:19:20 -0400

    accordion for homepage complete

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,11 @@
 import { useGetAllUsersQuery } from "../app/mainSlice";
 import { useState } from "react";
 import { Button } from "react-bootstrap";
+import { Container, Accordion } from "react-bootstrap";
+import InfoModal from "./Modal";
 
 const Home = () => {
   const { data, isLoading, isError, error } = useGetAllUsersQuery();  // get all users
-  console.log(data);
-
 
     // Modal logic
     const [response, setResponse] = useState();
@@ -35,22 +35,28 @@ const Home = () => {
               <></>
             )}
         <h2>Our users</h2>
-          <div className="user-list">
-            {data?.length > 0 ? (
-            data.map((user) => (
-                <div
-                key={user.id}
-                className="user-info"
-                >
-            <h3 className="user-info">{user.email}{user.firstName}{user.lastname}</h3>
-            <Button variant="primary">Update User</Button>
-            <Button variant="primary">Delete User</Button>
-            </div>
-            ))
-        ) : (
-            <h3> No users found </h3>
-        )}
-          </div>
+              <Accordion>
+                <div className="user-list">
+                    {data ?(
+                    data?.map((user) => (
+                      <div
+                        key={user.id}
+                        className="user-info"
+                       >
+                        <Accordion.Item eventKey={user.id}>
+                          <Accordion.Header className="user-info">{user.email} {user.firstName} {user.lastName}</Accordion.Header>
+                            <Accordion.Body>
+                              <Button variant="primary">Update User</Button>
+                              <Button variant="primary">Delete User</Button>
+                            </Accordion.Body>
+                        </Accordion.Item>
+                      </div>
+                    ))
+                  ) : (
+                      <h3> No users found </h3>
+                    )}
+                </div>
+              </Accordion>  
     </article>
   );
 };
