99f6d4ed8cb727cb33e269811463e7ebe45fec94
Ryan <ryfly2016@gmail.com>
2025-05-02 09:18:04 -0600

    add loading indicators to buttons

--- src/components/Registration.jsx
+++ src/components/Registration.jsx
@@ -12,7 +12,7 @@ import InfoModal from "./Modal";
 export default function Login() {
   const navigate = useNavigate();
 
-  const [register] = useRegisterMutation();
+  const [register, status] = useRegisterMutation();
 
   // Modal logic
   const [response, setResponse] = useState();
@@ -116,9 +116,15 @@ export default function Login() {
                 onChange={update}
               />
             </Form.Group>
-            <Button variant="primary" type="submit">
-              Submit
-            </Button>
+            {!status?.isLoading ? (
+              <Button variant="primary" type="submit">
+                Submit
+              </Button>
+            ) : (
+              <Button variant="primary" type="submit" disabled>
+                Loading...
+              </Button>
+            )}
           </Form>
         </Card.Body>
       </Card>
