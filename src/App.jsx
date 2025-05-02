412039919d90007196b3a4b62d037f5d56f5c334
Tor <Torskaa@gmail.com>
2025-05-02 16:21:44 -0400

    Working on it

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { Routes, Route } from "react-router-dom";
 // import Home from "./components/Home";
 import Login from "./components/Login";
 // import Registration from "./components/Registration";
-// import SingleUser from "./components/SingleUser";
+import SingleUser from "./components/SingleUser";
 
 function App() {
   return (
@@ -13,10 +13,9 @@ function App() {
         {/* No login needed */}
         {/* <Route path="/register" element={<Registration />} /> */}
         <Route path="/login" element={<Login />} />
-
         {/* must be logged in */}
-        {/* <Route path="/" element={<Home />} />
-      <Route path="/user/:id" element={<SingleUser />} /> */}
+        {/* <Route path="/" element={<Home />} /> */}
+        <Route path="/user/:id" element={<SingleUser />} />
       </Routes>
     </>
   );
