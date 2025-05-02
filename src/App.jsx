b1cca4c20a2061f8b6ba0578d14364acd88fc29b
Ryan <ryfly2016@gmail.com>
2025-05-02 09:04:40 -0600

    uncomment out some stuff

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Routes, Route } from "react-router-dom";
 
-// import Home from "./components/Home";
-// import Login from "./components/Login";
-// import Registration from "./components/Registration";
+import Home from "./components/Home";
+import Login from "./components/Login";
+import Registration from "./components/Registration";
 // import SingleUser from "./components/SingleUser";
 
 function App() {
@@ -11,12 +11,12 @@ function App() {
     <>
       <Routes>
         {/* No login needed */}
-        {/* <Route path="/register" element={<Registration />} /> */}
+        <Route path="/register" element={<Registration />} />
         <Route path="/login" element={<Login />} />
 
         {/* must be logged in */}
-        {/* <Route path="/" element={<Home />} />
-      <Route path="/user/:id" element={<SingleUser />} /> */}
+        <Route path="/" element={<Home />} />
+        {/* <Route path="/user/:id" element={<SingleUser />} /> */}
       </Routes>
     </>
   );
