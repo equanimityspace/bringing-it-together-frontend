5c58ddc92ec61f2b2b6dcfa105f67b1168d1fb33
Ryan <ryfly2016@gmail.com>
2025-05-02 07:08:45 -0600

    create Login.jsx

---
+++ src/components/Login.jsx
@@ -0,0 +1,89 @@
+import { useNavigate } from "react-router-dom";
+import { useLoginMutation } from "../app/mainSlice";
+import { useState } from "react";
+
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import Nav from "react-bootstrap/Nav";
+import Card from "react-bootstrap/Card";
+
+export default function Login() {
+  const navigate = useNavigate();
+
+  const [login] = useLoginMutation();
+
+  // stores data from login form
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  // store form data as it is typed
+  const update = (e) => {
+    setFormData((prev) => ({
+      ...prev,
+      [e.target.name]: e.target.value,
+    }));
+    console.log(e.target.name);
+  };
+
+  // submit login request
+  const submit = async (e) => {
+    try {
+      e.preventDefault();
+      console.log(formData);
+      const response = await login(formData);
+      // setResponse(response) for possible modal usage
+
+      // on successful login, return home
+      if (response?.data) {
+        console.log("successful login!");
+        navigate("/");
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <div className="d-flex justify-content-center vh-80">
+      <Card className="w-50 mt-5">
+        <Card.Header>
+          <Nav variant="tabs" defaultActiveKey="/login">
+            <Nav.Item>
+              <Nav.Link href="/login">Login</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link href="/register">Register</Nav.Link>
+            </Nav.Item>
+          </Nav>
+        </Card.Header>
+        <Card.Body>
+          <Form onSubmit={submit}>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control
+                type="email"
+                name="email"
+                placeholder="Enter email"
+                onChange={update}
+              />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control
+                type="password"
+                name="password"
+                placeholder="Password"
+                onChange={update}
+              />
+            </Form.Group>
+            <Button variant="primary" type="submit">
+              Submit
+            </Button>
+          </Form>
+        </Card.Body>
+      </Card>
+    </div>
+  );
+}
