7b87ffc1ca5e82dba00ae3ec3d4cfd4139bed825
Tor-Skaarva <torskaa@gmail.com>
2025-05-05 10:14:44 -0400

    Update Home.jsx
    changed navigation to /user

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -70,7 +70,7 @@ export default function Home() {
                         <Accordion.Item eventKey={user.id}>
                           <Accordion.Header className="user-info">{user.email} {user.firstName} {user.lastName}</Accordion.Header>
                             <Accordion.Body>
-                              <Button variant="primary" onClick={() => navigate(`getUser/${user.id}`)}>Update User</Button>
+                              <Button variant="primary" onClick={() => navigate(`user/${user.id}`)}>Update User</Button>
                               <Button variant="primary" onClick={() => handleDelete(user?.id)}>Delete User</Button>
                             </Accordion.Body>
                         </Accordion.Item>
